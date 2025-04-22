e29306f2369c3a4a6161ad6e9db93d024747b3b0
savchenko-maksym <maxim.savchenko91@gmail.com>
2025-04-22 23:51:47 +0300

    fixed query

--- src/App.jsx
+++ src/App.jsx
@@ -11,7 +11,7 @@ import ImageModal from "./components/ImageModal/ImageModal";
 
 function App() {
   const [images, setImages] = useState([]);
-  const [query, setQuery] = useState("green");
+  const [query, setQuery] = useState("");
   const [page, setPage] = useState(1);
   const [isLoading, setIsLoading] = useState(false);
   const [totalPages, setTotalPages] = useState(0);
